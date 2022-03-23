83bc53747ab9c9bfcd463f58219acfda83dcdb31
Andrey Kozhevnikov <andrey.kozhevnikov@myoffice.team>
2022-03-23 16:26:23 +0200

    Ignore QQuickRectangle in introspection

--- mytreemodel2.cpp
+++ mytreemodel2.cpp
@@ -498,6 +498,7 @@ QModelIndex MyTreeModel2::searchByCoordinates(qreal posx, qreal posy, TreeItem2*
             classname != QLatin1String("RotatingItem") &&
             classname != QLatin1String("QQuickShaderEffect") &&
             classname != QLatin1String("QQuickOverlay") &&
+            classname != QLatin1String("QQuickRectangle") &&
             classname != QLatin1String("QQuickShaderEffectSource") &&
             classname != QLatin1String("HwcImage") &&
             !classname.endsWith(QLatin1String("Gradient")) && posx >= itemx &&
